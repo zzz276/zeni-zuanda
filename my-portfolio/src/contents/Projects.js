446d3ee0312be1094bf284a3faa6a4d8926decab
Zeni Zuanda <zzeni2125@gmail.com>
2025-11-14 14:51:54 +0700

    Update Portfolio

--- my-portfolio/src/contents/Projects.js
+++ my-portfolio/src/contents/Projects.js
@@ -8,6 +8,7 @@ function Projects() {
     new Project("Defense of The Willi", "Java", "https://1drv.ms/u/c/e5956a9184ccc1cb/EVI0M6-r9ghHhv9akhaHWFIBOeV0bOzjqMj-m9la5DFg5w?e=IOrqsd"),
     new Project("DoJo Movie", "Kotlin", "https://1drv.ms/u/c/e5956a9184ccc1cb/EeU_7TlhEPtBh0fJXuBrkv4BAcnqqt0mfyTv3qUpgAjI1A?e=kAOTKT"),
     new Project("What Generator", "Kotlin", "https://1drv.ms/u/c/e5956a9184ccc1cb/EWq0pqMtdStKtflvrhYStXIBAgLzNoPHovF3G_f_O7CtKQ?e=Eu6G59"),
+    new Project("GPS-Tracker", "Arduino", "https://github.com/zzz276/GPS-Tracker"),
   ];
 
   return (
@@ -26,6 +27,9 @@ function Projects() {
           </Col>
           <Col>
             <Cards project={ projects[3] }/>
+          </Col>
+          <Col>
+            <Cards project={ projects[4] }/>
           </Col>
         </Row>
       </Container>
